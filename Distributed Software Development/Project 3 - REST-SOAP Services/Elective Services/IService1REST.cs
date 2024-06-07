5265009af14cc86e9d0d4810e6dbe38159dba8a2
SSVNormandySR2 <112334701+SSVNormandySR2@users.noreply.github.com>
2024-06-07 07:46:38 -0700

    Add files via upload

---
+++ Distributed Software Development/Project 3 - REST-SOAP Services/Elective Services/IService1REST.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Vincent Latona
+ * Elective RESTful Counting Services
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ElectiveWordServices
+{
+    /*
+     * This RESTful service wraps 2 easy services into a single service
+     */
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        [WebGet(UriTemplate = "StringWordCount?input={input}")] //RESTful Template
+        int StringWordCount(string input); //Easy service 1 - Word Counter
+
+        [OperationContract]
+        [WebGet(UriTemplate = "CharacterCount?input={input}")] //RESTful Template
+        int CharacterCount(string input); //Easy service 2 - Character Counter
+    }
+}
---
+++ Distributed Software Development/Project 3 - REST-SOAP Services/Elective Services/Service1REST.svc.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Vincent Latona
+ * Elective RESTful Counting Services
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ElectiveWordServices
+{
+    /*
+     * This RESTful service implementation implements 2 individual easy services
+     */
+    public class Service1 : IService1
+    {
+        /*
+         * This RESTful service counts the number of words in a given input string
+         */
+        public int StringWordCount(string input)
+        {
+            int wordCount = 0; //Initialize count to 0
+            string[] words = input.Split(' '); //Split string into words
+            wordCount = words.Length; //Get number of words
+            return wordCount; //Return total word count
+        }
+
+        /*
+         * This RESTful service counts the number of characters in a given input string
+         */
+        public int CharacterCount(string input)
+        {
+            int characterCount = 0; //Initialize count to 0
+            foreach(char stringChar in input) //Iterate over characters in the string
+            {
+                characterCount++; //Increment character count
+            }
+            return characterCount; //Return total character count
+        }
+    }
+}
